986f0d21d77c528ccee0d5bdbb78fcd902315c13
liuam <liuam@usoftchina.com>
2018-11-07 18:49:22 +0800

    Initial commit

---
+++ grpc-stream/src/main/java/com/lam/demo/grpc/stream/GrpcServer.java
@@ -0,0 +1,17 @@
+package com.lam.demo.grpc.stream;
+
+import io.grpc.Server;
+
+/**
+ * @author liuam
+ * @since 2018/11/7 0007 下午 17:51
+ */
+public class GrpcServer {
+
+    private Server server;
+
+    private void start() throws Exception {
+
+    }
+
+}
